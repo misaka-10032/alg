da41e9e0af84c483252bb1f08d1e407a1e434ba6
Longqi Cai <longqicai@gmail.com>
2016-08-05 12:43:17 -0400

    fix dependencies

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Algorithm practice
 
 Self-paced learning of [6.006](https://www.youtube.com/playlist?list=PLUl4u3cNGP61Oq3tWYp6V_F-5jb5L2iHb).
+
+[![Build Status](https://travis-ci.org/misaka-10032/alg.svg?branch=master)](https://travis-ci.org/misaka-10032/alg)
--- setup.py
+++ setup.py
@@ -2,12 +2,18 @@
 # encoding: utf-8
 
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
+
+install_reqs = parse_requirements('requirements.txt', session=False)
+reqs = [str(ir.req) for ir in install_reqs]
+
 
 setup(name='alg',
       version='0.1',
-      description='Common algorithms',
+      description='Commonly used algorithms',
       author='Misaka-10032',
       author_email='longqic@andrew.cmu.edu',
       url='https://github.com/misaka-10032/alg',
       packages=find_packages(),
+      install_requires=reqs,
       )
